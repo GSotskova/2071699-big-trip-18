104370cb4f27a2c68124388f808e3cadc0fba536
GSotskova <lgv11.09@gmail.com>
2022-09-22 21:45:16 +0300

    Пришёл, увидел, загрузил (часть 1)

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -32,8 +32,7 @@ const createPhotoTemplate = (photos) => {
     photosImgTemplate += `<img class="event__photo" src="${el.src}" alt="Event photo"></img>`;
   });
   return photosImgTemplate;
-
-
+};
 
 const createDestinationTemplate = (description, photoDestination) => (
   `<section class="event__section  event__section--destination">
